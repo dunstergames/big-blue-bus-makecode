e9393358134e17a0bdce1b0e6daf73e441c29d5f
Callum Dunster <cdunster@users.noreply.github.com>
2020-09-21 20:17:45 +0200

    Update ice cream art.

--- main.ts
+++ main.ts
@@ -80,22 +80,38 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (iceCreamCount < 3) {
         iceCreamCount += 1
         iceCream = sprites.createProjectileFromSprite(img`
-            . . . . . 3 3 b 3 3 d d 3 3 . . 
-            . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
-            . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
-            . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
-            . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
-            . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
-            . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
-            . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
-            . 4 4 d 1 1 1 1 1 1 d d d b b . 
-            . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
-            4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
-            4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
-            4 5 5 d 5 5 d b b b d d 3 . . . 
-            4 5 5 5 d d d d 4 4 b 3 . . . . 
-            . 4 5 5 5 4 4 4 . . . . . . . . 
-            . . 4 4 4 . . . . . . . . . . . 
+            .....fff.......
+            ....fbf........
+            ...fbb1ff......
+            ..fbb1111f.....
+            .fbb111b11f....
+            .fb111b1f11f...
+            .f111fffb111f..
+            f1fffbb111111f.
+            f1bb1111111fff.
+            f111111ffffb1f.
+            111ffffbbb1111f
+            f111bbb1111111f
+            f111111111111bf
+            bf111111ffff1f.
+            11111fffbbb1bf.
+            fffffb11111bf..
+            fbbb111bbbbf4f.
+            .ffffbbffff4df.
+            .f4dfffd444d4f.
+            .fd444d4d4d44f.
+            ..fd4d444d44f..
+            ..f4d444d4d4f..
+            ..fd4d4d444df..
+            ...f44d444df...
+            ...f4d4d4d4f...
+            ...fd444d44f...
+            ....f44d4df....
+            ....f4d444f....
+            .....f444f.....
+            .....fd44f.....
+            ......fdf......
+            .......f.......
             `, bus, 0, -50)
         iceCream.setKind(SpriteKind.IceCream)
     }
